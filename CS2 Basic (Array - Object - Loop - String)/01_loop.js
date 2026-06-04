842c2a4d89822158c4f8658d2554f78fbad653e8
bayjidalom <bayjidalom1005@gmail.com>
2026-06-04 23:59:13 +0600

    loop

---
+++ CS2 Basic (Array - Object - Loop - String)/01_loop.js
@@ -0,0 +1,33 @@
+// for loop
+for (let i = 0; i <= 10; i++) {
+    console.log("Looping.....")
+}
+
+
+// while loop
+let x = 10;
+while (x <= 20) {
+    if (x % 2 === 0) {
+        console.log("Even number :", x)
+    }
+    x++;
+}
+
+
+// let i = 0;
+// while (true) {
+//     console.log("Infinite looping.......")
+//     i++;
+// }
+
+
+
+// do while loop (first:execute   second:conditin check)
+let n = 0;
+do {
+    console.log(n)
+    n++
+}
+while (n <= 5)
+
+
