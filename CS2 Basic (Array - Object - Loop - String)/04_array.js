04efd3766fc1427a5529543173bf2342a29224a7
bayjidalom <bayjidalom1005@gmail.com>
2026-06-05 22:24:29 +0600

    array - index base

---
+++ CS2 Basic (Array - Object - Loop - String)/04_array.js
@@ -0,0 +1,34 @@
+// Array - index base [index start from 0]
+
+let numbers = [10, 20, 30, 40, 50, 60];
+console.log(numbers)
+
+// Element access
+console.log("Index 5 Value :", numbers[5])
+
+// count elements (How many elements in array. To find size of an array.)
+console.log("Length of the array is :", numbers)
+
+
+// added last
+numbers.push(100, 110)
+console.log(numbers)
+
+// remove last item
+numbers.pop()
+numbers.pop()
+console.log(numbers)
+
+// added first
+numbers.unshift(500)
+numbers.unshift(400)
+console.log(numbers)
+
+// remove from first
+numbers.shift()
+numbers.shift()
+console.log(numbers)
+
+
+
+
