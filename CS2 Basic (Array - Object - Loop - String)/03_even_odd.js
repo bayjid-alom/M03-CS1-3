9442d95051024d01aeb4e81efa2bff7c7661a6e3
bayjidalom <bayjidalom1005@gmail.com>
2026-06-05 22:10:58 +0600

    even_odd

---
+++ CS2 Basic (Array - Object - Loop - String)/03_even_odd.js
@@ -0,0 +1,34 @@
+// even numbers
+
+for (let i = 30; i <= 40; i++) {
+    if (i % 2 === 0) {
+        console.log("Even - (30-40) :", i)
+    }
+}
+
+
+let i = 10;
+while (i <= 20) {
+    if (i % 2 === 0)
+        console.log("Even | (10-20) :", i)
+    i++;
+}
+
+
+
+// odd numbers
+
+for (let x = 70; x <= 80; x++) {
+    if (x % 2 === 1) {
+        console.log("Odd - (70-80) :", x)
+    }
+}
+
+let x = 90;
+while (x <= 100) {
+    if (x % 2 === 1) {
+        console.log("Odd | (90-100) :", x);
+    }
+    x++;
+}
+
